0c99d93b5d2bd3d359b04a53f3ab5dcbf7d1f89a
1vec <29864621+1vec@users.noreply.github.com>
2018-05-14 19:14:49 +0800

    Update init_wordpress.sh

--- init_wordpress.sh
+++ init_wordpress.sh
@@ -5,7 +5,7 @@
 #    initialize VPS on Ubuntu 16.04 through bash operations.
 yum update -y
 yum upgrade -y
-yum install gcc zsh gdb git mariadb-server php-mysql httpd wordpress -y
+yum install gcc zsh gdb git mariadb-server php php-fpm php-mysql httpd wordpress -y
 chsh -s /bin/zsh
 systemctl start httpd
 systemctl enable httpd
@@ -15,3 +15,5 @@ systemctl start mariadb
 systemctl enable mariadb
 firewall-cmd --add-service=http --zone=public --permanent
 firewall-cmd --reload
+rm -rf /var/www/html
+ln -s /usr/share/wordpress /var/www/html
