e1753d297f412744988950d0bb0179fdf5410265
1vec <29864621+1vec@users.noreply.github.com>
2018-05-14 14:09:37 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,15 +5,15 @@
 #    initialize VPS on Ubuntu 16.04 through bash operations.
 apt update
 apt upgrade -y
-apt install gdb git -y
+apt install gcc zsh gdb git -y
 username=username
 if [ -z "$username" ]; then
 	exit 0
 fi
-useradd -m -s /bin/bash ${username}
+useradd -m -s /bin/zsh ${username}
 passwd ${username}
 echo -e "${username}\tALL=(ALL:ALL)\tALL">>/etc/sudoers
 echo -e "${username}\tALL=NOPASSWD:ALL">>/etc/sudoers
 cp -r .ssh /home/${username}
-chown ${username}:${username} /home/${username}/.ssh
-chown ${username}:${username} /home/${username}/.ssh/authorized_keys
+chown -r ${username}:${username} /home/${username}/.ssh
+#chown ${username}:${username} /home/${username}/.ssh/authorized_keys
